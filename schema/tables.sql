cdf52553edb83b00c7bce47ccea26d2645151e28
Jordan Schalm <jordan.schalm@gmail.com>
2017-06-21 18:24:40 -0700

    fix schema

--- schema/tables.sql
+++ schema/tables.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 DROP TABLE IF EXISTS members;
 CREATE TABLE members (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    email VARCHAR(128) PRIMARY KEY,
+    email VARCHAR(128),
     first_name VARCHAR(128),
     last_name VARCHAR(128),
     github_username VARCHAR(128),
@@ -15,7 +15,7 @@ CREATE TABLE members (
 DROP TABLE IF EXISTS teams;
 CREATE TABLE teams (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name VARCHAR(128) PRIMARY KEY,
+    name VARCHAR(128),
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (now() at time zone 'utc')
 );
 
