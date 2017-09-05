73a3458b342f2449b94d7d327e9406ad1456aafc
Jordan Schalm <jordan.schalm@gmail.com>
2017-09-04 23:31:54 -0700

    test

--- model/team.go
+++ model/team.go
@@ -11,8 +11,8 @@ type Team struct {
 	TableName struct{} `sql:"teams" json:"-"`
 
 	Name           string    `sql:",pk" json:"name"`
-	GithubTeamName string    `sql:"github_team_name" json:"-"`
-	GithubTeamID   int       `sql:"github_team_id" json:"-"`
+	GithubTeamName string    `pg:"github_team_name" json:"-"`
+	GithubTeamID   int       `pg:"github_team_id" json:"-"`
 	CreatedAt      time.Time `json:"-"`
 
 	Members []*Member `sql:"-" json:"members" pg:",many2many:team_members,joinFK:Member"`
