5f8413044de5b8097355f877d1e378da8c9f2ea8
Bruno Bachmann <bfbachmann@gmail.com>
2018-01-21 00:06:56 -0800

    Switch to docker-compose setup

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,22 @@
 # Start from the base Go image
 FROM golang
 
+# Set /go/src/github.com/ubclaunchpad/rocket as the CWD
+WORKDIR /go/src/github.com/ubclaunchpad/rocket
+
+# Install Postgres client to check when the DB is ready for use
+RUN apt-get update
+RUN apt-get install -f -y postgresql-client
+
 # Copy package source files to container
-ADD . /go/src/github.com/ubclaunchpad/rocket
+ADD . .
 
 # Download and install dependency manager
 RUN go get github.com/Masterminds/glide
 RUN go install github.com/Masterminds/glide
 
 # Install dependencies
-RUN cd /go/src/github.com/ubclaunchpad/rocket && \
-    glide install
+RUN glide install
 
 # Build Rocket
 RUN go install github.com/ubclaunchpad/rocket
-
-# Run
-ENTRYPOINT [ "/go/bin/rocket" ]
--- start.sh
+++ start.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-# Build the container from the project directory and tag as latest.
-docker build \
-    -t rocket-app:latest \
-    /go/src/github.com/ubclaunchpad/rocket
+# Wait for postgres instance to boot before starting Rocket.
 
-# Stop the old container, if it's running.
-docker stop rocket-app
+set -e
+echo "Waiting for Postgres to start..."
 
-# Remove the old container, so we can start a new one.
-docker rm rocket-app
+while true; do
+  if eval "pg_isready -h postgres -p 5432"; then
+    break
+  fi
+  sleep 5
+done
 
-# Run Rocket. Environment variables are populated from .app.env and define the
-# app configuration. The container is attached to a user-defined network so it
-# can reach the database (which is on the same network). Port 80 is bound to
-# the host so we can reach the API.
-docker run \
-    --name rocket-app \
-    --env-file /go/src/github.com/ubclaunchpad/rocket/.app.env \
-    --network rocket-net \
-    -p 80:80 \
-    -d \
-    rocket-app
+echo "Postgres ready"
+eval rocket
